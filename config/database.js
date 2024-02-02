0810551328dd07287bf67b1cb5bba296be376789
unknown <phucphuc1304>
2024-02-02 11:44:42 +0700

    first commit

---
+++ config/database.js
@@ -0,0 +1,25 @@
+
+//module.exports = {
+//	'url': 'mongodb://127.0.0.1:27017',
+//	'options': {
+//		'user':   'root',
+//		'pass':   '',
+//		'dbName': 'PlayGameChuan2',
+//		'useNewUrlParser': true,
+//		'useUnifiedTopology': true,
+//		//'autoIndex':       false,
+//	},
+//};
+
+
+module.exports = {
+	'url': 'mongodb+srv://phucprodl3:phucdk113@cluster0.pig9afy.mongodb.net',
+	'options': {
+	//	'user':   'root',
+	//	'pass':   '',
+		'dbName': 'PlayGame', // red
+		'useNewUrlParser': true,
+		'useUnifiedTopology': true,
+		//'autoIndex':       false,
+	},
+};
